afefe389dd8e352147dc13caa758cd7f02d0730e
shamerdanov <shamil332211@list.ru>
2024-06-24 14:37:34 +0400

    add repository class

---
+++ app/src/main/java/com/example/mini_application/data/repository/CoinRepository.kt
@@ -0,0 +1,15 @@
+package com.example.mini_application.data.repository
+
+import com.example.mini_application.data.api.ApiService
+import com.example.mini_application.data.model.CoinModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.Response
+import javax.inject.Inject
+
+class CoinRepository @Inject constructor(private val apiService: ApiService) {
+
+    suspend fun getAllCity(): Response<CoinModel> = withContext(Dispatchers.IO) {
+        return@withContext apiService.getAllCity()
+    }
+}
