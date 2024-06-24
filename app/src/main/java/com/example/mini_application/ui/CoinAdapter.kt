6768dd697ed126772d9d9987512b2241dcbf8b7c
shamerdanov <shamil332211@list.ru>
2024-06-24 15:20:03 +0400

    init

---
+++ app/src/main/java/com/example/mini_application/ui/CoinAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.mini_application.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mini_application.R
+import com.example.mini_application.data.model.Data
+import com.example.mini_application.databinding.CoinItemBinding
+
+class CoinAdapter : RecyclerView.Adapter<CoinAdapter.CoinViewHolder>() {
+
+    private val listOfCoin = ArrayList<Data>()
+
+    inner class CoinViewHolder(val coinItem: CoinItemBinding) :
+        RecyclerView.ViewHolder(coinItem.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoinViewHolder {
+        val view = CoinItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CoinViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return listOfCoin.size
+    }
+
+    override fun onBindViewHolder(holder: CoinViewHolder, position: Int) {
+        holder.coinItem.item = listOfCoin[position]
+        when (listOfCoin[position].fullName){
+            R.drawable.coin1
+        }
+    }
+
+    fun updateList(newList: List<Data>) {
+        listOfCoin.clear()
+        listOfCoin.addAll(newList)
+        notifyDataSetChanged()
+    }
+}
---
+++ app/src/main/java/com/example/mini_application/ui/HomeFragment.kt
@@ -0,0 +1,13 @@
+package com.example.mini_application.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mini_application.R
+
+class HomeFragment : Fragment() {
+
+
+}
