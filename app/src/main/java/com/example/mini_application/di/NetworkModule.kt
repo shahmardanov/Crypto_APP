3582fbdf3aa5a489dd188658bc6de106bb6e4f04
shamerdanov <shamil332211@list.ru>
2024-06-24 14:39:22 +0400

    add constand and NetworkModule class

---
+++ app/src/main/java/com/example/mini_application/di/NetworkModule.kt
@@ -0,0 +1,32 @@
+package com.example.museumsinturkey.di
+
+import com.example.mini_application.data.api.ApiService
+import com.example.museumsinturkey.utill.Constant
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(Constant.BASE_URL + Constant.API_REGION)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideAPIServices(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+
+}
